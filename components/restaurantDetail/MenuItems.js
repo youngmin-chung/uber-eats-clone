e108159fb523307fafad357f292895d65ddc7f01
Youngmin Chung <dev.youngmin@gmail.com>
2021-10-22 12:06:49 -0400

    feature_viewcart_totalprice

--- components/restaurantDetail/MenuItems.js
+++ components/restaurantDetail/MenuItems.js
@@ -59,10 +59,14 @@ const styles = StyleSheet.create({
 
 export default function MenuItems(restaurantName) {
   const dispatch = useDispatch()
-  const selectItem = (item) =>
+  const selectItem = (item, checkboxValue) =>
     dispatch({
       type: 'ADD_TO_CART',
-      payload: { ...item, restaurantName: restaurantName },
+      payload: {
+        ...item,
+        restaurantName: restaurantName,
+        checkboxValue: checkboxValue,
+      },
     })
 
   return (
@@ -76,7 +80,7 @@ export default function MenuItems(restaurantName) {
                 borderRadius: 0,
               }}
               fillColor='green'
-              onPress={() => selectItem(food)}
+              onPress={(checkboxValue) => selectItem(food, checkboxValue)}
             />
             <FoodInfo food={food} />
             <FoodImage food={food} />
--- components/restaurantDetail/ViewCart.js
+++ components/restaurantDetail/ViewCart.js
@@ -1,37 +1,64 @@
 import React from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
+import { useSelector } from 'react-redux'
 
 export default function ViewCart() {
+  const items = useSelector((state) => state.cartReducer.selectedItems.items)
+  // '$9.99' -> '9.99' -> Number('9.99') -> 9.99
+  // it contains [9.99, 10.99, 11.99]
+  // reduce -> [9.99, 10.99, 11.99]
+  // reduce -> 9.99 + 10.99 + 11.99
+  const total = items
+    .map((item) => Number(item.price.replace('$', '')))
+    .reduce((prev, curr) => prev + curr, 0)
+
+  const totalUSD = total.toLocaleString('en', {
+    style: 'currency',
+    currency: 'USD',
+  })
+
+  console.log(totalUSD)
+
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'row',
-        position: 'absolute',
-        bottom: 220,
-        zIndex: 999,
-      }}>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'center',
-          width: '100%',
-        }}>
-        <TouchableOpacity
+    <>
+      {total ? (
+        <View
           style={{
-            marginTop: 20,
-            backgroundColor: 'black',
+            flex: 1,
             alignItems: 'center',
-            padding: 13,
-            borderRadius: 30,
-            width: 300,
-            position: 'relative',
+            justifyContent: 'center',
+            flexDirection: 'row',
+            position: 'absolute',
+            bottom: 220,
+            zIndex: 999,
           }}>
-          <Text style={{ color: 'white', fontSize: 20 }}>VIEW CART</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'center',
+              width: '100%',
+            }}>
+            <TouchableOpacity
+              style={{
+                marginTop: 20,
+                backgroundColor: 'black',
+                flexDirection: 'row',
+                justifyContent: 'flex-end',
+                padding: 15,
+                borderRadius: 30,
+                width: 300,
+                position: 'relative',
+              }}>
+              <Text style={{ color: 'white', fontSize: 20, marginRight: 30 }}>
+                View Cart
+              </Text>
+              <Text style={{ color: 'white', fontSize: 20 }}>{totalUSD}</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      ) : (
+        <></>
+      )}
+    </>
   )
 }
